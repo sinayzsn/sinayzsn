7caf141d2750f005e5edd2a90b9436ffd7528f54
Sinayzsn <89442786+sinayzsn@users.noreply.github.com>
2023-07-30 10:37:29 +0330

    Update use-python-3.10.sh

--- tools/use-python-3.10.sh
+++ tools/use-python-3.10.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Add deadsnakes PPA repository
-sudo add-apt-repository ppa:deadsnakes/ppa
+sudo add-apt-repository ppa:deadsnakes/ppa -y
 
 # Update package list
-sudo apt-get update
+sudo apt-get update -y
 
 # Install Python 3.10
-sudo apt-get install python3.10
+sudo apt-get install python3.10 -y
 
 # Update alternatives to make Python 3.10 the default version
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
